45f317a23dba0e19b34994c2fbb381235d864d2a
alexmeia <alexmeia@gmail.com>
2017-09-25 12:45:21 +0200

    Added URL handler at application statrt

--- app/app.ts
+++ app/app.ts
@@ -5,11 +5,18 @@ purpose of the file is to pass control to the app’s first module.
 */
 import * as app from "application";
 import "./bundle-config";
-import { handleOpenURL, AppURL } from 'nativescript-urlhandler';
-import { HomeViewModel } from "./home/home-view-model";
+import { handleOpenURL, AppURL } from "nativescript-urlhandler";
 
+handleOpenURL((appURL: AppURL) => {
+    this.callbackUrl = appURL.toString();
+    this.set("testUrl", appURL.toString());
+    console.log("App URL: " + this.callbackUrl);
 
+    // start signIn process with Oidc Client
+});
 app.start({ moduleName: "home/home-page" });
+
+
 
 /*
 Do not place any code after the application has been started as it will not
