c38998fb80952373b44783de0d91c60e121732f3
alexmeia <alessandro.meiattini@phoops.it>
2017-09-27 17:09:52 +0200

    Switch to authorization code flow

--- app/login/login-view-model.ts
+++ app/login/login-view-model.ts
@@ -10,9 +10,9 @@ export class LoginViewModel extends Observable {
         let authority = "http://keycloak-dev.phoops.it:9876/auth/realms/mint/protocol/openid-connect/auth";
 
         let oidcParams = {
-            client_id: "vuejs-sample-client",
+            client_id: "nativescript-sample-client",
             redirect_uri: "http://localhost:8080/static/callback",
-            response_type: "id_token token",
+            response_type: "code",
             scope: "openid profile all_claims",
             state: "56026239d44b4e678a4b56408da657e9", // TODO:  retrieve from request object
             nonce: "0e36d3cf3e954b798d7233766b257f73"
